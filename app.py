a8dcc43b63ef026e210e1c55c8d33d4fcee6b8da
2137942shubham <144312627+2137942shubham@users.noreply.github.com>
2024-08-22 21:38:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,5 +38,5 @@ def knowledge_base_response():
             return jsonify({'error': f"API Request Error: {e}"}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == "__main__":
+    app.run()
